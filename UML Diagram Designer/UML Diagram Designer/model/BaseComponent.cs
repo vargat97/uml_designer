039ee4ea16962fb6342d3408d3660f9f42faa86d
vargat97 <47600444+vargat97@users.noreply.github.com>
2021-09-11 10:00:05 +0200

    Create the base of the mediator pattern

---
+++ UML Diagram Designer/UML Diagram Designer/model/BaseComponent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UML_Diagram_Designer.model
+{
+    //TODO
+    public class BaseComponent
+    {
+        protected IMediator mediator;
+
+        public BaseComponent(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+       
+    }
+}
---
+++ UML Diagram Designer/UML Diagram Designer/model/IMediator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UML_Diagram_Designer.model
+{
+    interface Mediator
+    {
+    }
+}
