24b5ee3fffa630768c17db2e1bd005ebd40463bb
vargat97 <47600444+vargat97@users.noreply.github.com>
2021-11-13 19:05:36 +0100

    add test base functions

---
+++ UML_Diagram_Designer/UML_Diagram_Designer_Test/DiagramEditorViewModelTest.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UML_Diagram_Designer_Test
+{
+    [TestClass]
+    public class DiagramEditorViewModelTest
+    {
+        [TestMethod]
+        public void TestCreateClassMethod()
+        {
+           
+        }
+        [TestMethod]
+        public void TestCreateInterfaeMethod()
+        {
+
+        }
+        [TestMethod]
+        public void TestCreateEnumerationMethod()
+        {
+
+        }
+        [TestMethod]
+        public void TestCreateAttributeMethod()
+        {
+
+        }
+        [TestMethod]
+        public void TestCreateOperationMethod()
+        {
+
+        }
+        [TestMethod]
+        public void TestCreateParaeterMethod()
+        {
+
+        }
+        [TestMethod]
+        public void TestCreateEnumerationLiterralMethod()
+        {
+
+        }
+        [TestMethod]
+        public void TestCreateAssociationMethod()
+        {
+
+        }
+        [TestMethod]
+        public void TestCreateDependencyMethod()
+        {
+
+        }
+        [TestMethod]
+        public void TestCreateGeneralizationMethod()
+        {
+
+        }
+        [TestMethod]
+        public void TestCreateInterfaceRealizationMethod()
+        {
+
+        }
+        [TestMethod]
+        public void TestModifyObjectNameMethod()
+        {
+
+        }
+        [TestMethod]
+        public void TestModifyObjectVisibilityMethod()
+        {
+
+        }
+        [TestMethod]
+        public void TestRemoveClassMethod()
+        {
+
+        }
+        [TestMethod]
+        public void TestRemoveInterfaceMethod()
+        {
+
+        }
+        [TestMethod]
+        public void TestRemoveEnumerationMethod()
+        {
+
+        }
+        [TestMethod]
+        public void TestRemoveAttributeMethod()
+        {
+
+        }
+        [TestMethod]
+        public void TestRemoveOperationethod()
+        {
+
+        }
+        [TestMethod]
+        public void TestRemoveParameterMethod()
+        {
+
+        }
+        [TestMethod]
+        public void TestRemoveEnumrationLiteralMethod()
+        {
+
+        }
+        [TestMethod]
+        public void TestRemoveAssociationMethod()
+        {
+
+        }
+        [TestMethod]
+        public void TestRemoveDependencyMethod()
+        {
+
+        }
+        [TestMethod]
+        public void TestRemoveGeneralizationMethod()
+        {
+
+        }
+        public void TestRemoveInterfaceRealizationMethod()
+        {
+
+        }
+    }
+}
